99af8a6f2fe89b5bdd524c112b2ca7e3adb217c4
Sergio Escalona <sergio@foreach.cl>
2023-03-04 20:05:02 -0300

    XLsx

--- app/API/V1/modules/dashboard/routes.py
+++ app/API/V1/modules/dashboard/routes.py
@@ -6,6 +6,9 @@ from sqlalchemy.orm import contains_eager
 from sqlalchemy.orm.session import Session
 from sqlalchemy.sql.expression import or_, and_
 from fastapi_pagination import Params, Page
+from starlette.responses import StreamingResponse
+from io import BytesIO
+import xlsxwriter
 from app.database.main import get_database
 from ...services.CRUD import CrudService
 from ...helpers.schema import SuccessMessage
@@ -18,7 +21,7 @@ dash_service = CrudService(Model=Product)
 
 
 @router.get("/graphs")
-def get_all(
+def get_graphs(
     start_date: str = Query(None, alias="startDate"),
     end_date: str = Query(None, alias="endDate"),
     db: Session = Depends(get_database),
@@ -51,3 +54,74 @@ def get_all(
     )
 
     return products
+
+
+def products_to_xlsx(data):
+    try:
+        output = BytesIO()
+        workbook = xlsxwriter.Workbook(output)
+        title = workbook.add_format({"bold": True})
+        worksheet = workbook.add_worksheet("Productos")
+        worksheet.set_column(0, 1, 10)
+        worksheet.set_column(2, 3, 15)
+        worksheet.write("A1", "Categoría", title)
+        worksheet.write("B1", "Id", title)
+        worksheet.write("C1", "Total stock", title)
+        worksheet.write("D1", "Cant. productos", title)
+
+        row = 1
+        col = 0
+
+        for product in data:
+            worksheet.write(row, col, product["name"])
+            worksheet.write(row, col + 1, product["category_id"])
+            worksheet.write(row, col + 2, product["total"])
+            worksheet.write(row, col + 3, product["count"])
+
+            row += 1
+
+        workbook.close()
+        output.seek(0)
+
+        return output
+
+    except Exception as err:
+        print("Error message : {0}".format(err))
+
+
+@router.post("/xlsx")
+def get_xlsx(
+    start_date: str = Query(None, alias="startDate"),
+    end_date: str = Query(None, alias="endDate"),
+    db: Session = Depends(get_database),
+):
+    start_filter = []
+    end_filter = []
+
+    if start_date:
+        start_filter.append(Product.created_at >= start_date)
+
+    if end_date:
+        end_date = end_date + " 23:59:59"
+        end_filter.append(Product.created_at <= end_date)
+
+    products = (
+        db.query(
+            Product.category_id.label("category_id"),
+            ProductCategory.name,
+            func.sum(Product.stock).label("total"),
+            func.count(Product.stock).label("count"),
+        )
+        .join(ProductCategory)
+        .filter(
+            Product.is_deleted == False,
+            and_(*start_filter),
+            and_(*end_filter),
+        )
+        .group_by(Product.category_id, ProductCategory.name)
+        .all()
+    )
+    xlsx = products_to_xlsx(products)
+    headers = {"Content-Disposition": "attachment; filename=productos.xlsx"}
+
+    return StreamingResponse(xlsx, headers=headers)
